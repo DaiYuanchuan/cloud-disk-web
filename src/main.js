9684b399e3a0eb3f0945edab75d67ed480b8a126
Dan <novel-web@novelweb.cn>
2021-03-14 20:01:51 +0800

    修正 样式加载顺序

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
@@ -10,7 +8,9 @@ import VueAxios from 'vue-axios'
 import cookies from 'js-cookie'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
+import App from './App'
 import {asyncRoutes} from './router/routers'
+import router from './router'
 
 import './icons' // icon
 
